fd92127484eb1cd7f22ee92aaae1fd48604a962b
Ivan (w23) Avdeev <me@w23.ru>
2012-12-23 16:11:27 +0700

    SDL: added map limit

--- src/main_sdl.cpp
+++ src/main_sdl.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <errno.h>
 #include <kapusha/sys/SDL/KPSDL.h>
 #include <kapusha/core/Log.h>
 #include "OpenSource.h"
@@ -14,10 +16,21 @@ int main(int argc, char* argv[])
 {
   if (argc < 3)
   {
-    fprintf(stderr, "Usage: %s path_to_gcf/root/gamename first_map_name\n", argv[0]);
+    fprintf(stderr, "Usage: %s path_to_gcf/root/gamename first_map_name [map_count_limit(=128 by default)]\n", argv[0]);
     return 1;
   }
   KP_LOG_OPEN("OpenSource.log", new StderrLog);
-  return kapusha::KPSDL(new OpenSource(argv[1], argv[2], 128), 1280, 720);
+
+  int map_limit = 128;
+  if (argc > 3)
+  {
+  	map_limit = strtol(argv[3], 0, 0);
+	if (map_limit < 1) {
+		fprintf(stderr, "Invalid map count limit %s, must be a (reasonable) number > 0\n", argv[3]);
+		return EINVAL;
+	}
+  }
+
+  return kapusha::KPSDL(new OpenSource(argv[1], argv[2], map_limit), 1280, 720);
 }
 
