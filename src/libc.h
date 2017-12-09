9d2ada4a31cb5740bf56187c456ec569103e1c95
Ivan Avdeev <me@w23.ru>
2017-12-09 17:47:12 +0700

    fix windows build

--- src/libc.h
+++ src/libc.h
@@ -1,17 +1,20 @@
 #pragma once
 
-#include <alloca.h>
 #include <stdint.h>
 #include <stdio.h> /* printf */
 #include <stdlib.h> /* malloc */
 #include <stddef.h> /* offsetof, size_t */
 #include <string.h> /* memset */
 #ifndef _WIN32
+#include <alloca.h>
 #include <strings.h> /* strncasecmp */
 #else
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
+#undef near
+#undef far
 #define strncasecmp _strnicmp
+#define alloca _alloca
 #endif
 #include <ctype.h> /* isspace */
