a434e484c254a935f47d8cab7df3f9cd72f9420b
Ivan Avdeev <me@provod.works>
2024-01-10 21:45:35 -0500

    fixup stupid typos

--- src/log.c
+++ src/log.c
@@ -1,4 +1,3 @@
-#pragma once
 #include "log.h"
 
 #include <stdio.h>
--- src/log.h
+++ src/log.h
@@ -3,7 +3,7 @@
 void logOpen(const char *logfile);
 void logClose(void);
 
-void logPrintf(PRINTF_ARG(const char *format), ...) PRINTF_ATTR(0, 1);
+void logPrintf(PRINTF_ARG(const char *format), ...) PRINTF_ATTR(1, 2);
 
 #define PRINTF(fmt, ...) logPrintf(__FILE__ ":" STR(__LINE__) ": " fmt "\n", __VA_ARGS__)
 #define PRINT(msg) logPrintf(__FILE__ ":" STR(__LINE__) ": %s\n", msg)
